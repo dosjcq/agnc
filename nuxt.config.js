6bb0f18cc3fe9b22aade94efaa905043c2a1b7ee
pupsi4ek <sda2000den@gmail.com>
2021-09-30 16:26:09 +0300

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -93,7 +93,7 @@ export default {
   ],
 
   axios: {
-    baseURL: "http://195.140.147.103"
+    baseURL: "https://195.140.147.103"
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
